47c3c6aa1ee5a956f356f91a8156af980e34610c
your-username <singhkunal981@gmail.com>
2026-05-22 16:21:37 +0530

    hell route

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def hello():
     return "pipeline works"
 
+@app.route("/hell")
+def hello():
+    return "devOps is hell"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
