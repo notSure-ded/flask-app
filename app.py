9e2c15becd05d6facace37f1ed28c88e520b62e7
your-username <singhkunal981@gmail.com>
2026-05-22 15:59:38 +0530

    new route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Flask app deployed using Jenkins + ArgoCD + AKS"
 
+@app.route("/hello")
+def hello():
+    return "pipeline works"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
